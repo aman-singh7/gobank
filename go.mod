b9527116219dfe6b8b5723ca1f172df52786fc29
Aman <aman2@me.iitr.ac.in>
2023-05-12 23:40:21 +0530

    feat: intialize basic structure
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aman-singh7/gobank
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	server := NewAPIServer(":3000")
+	server.Run()
+
+	fmt.Println("Hey Buddy")
+}
